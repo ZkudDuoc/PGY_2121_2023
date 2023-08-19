19fa7f409ae508a666405f04a0a1ac082cde4d43
nico <nico.saavedram@duocuc.cl>
2023-08-19 10:00:14 -0400

    clase introduccion

---
+++ clase_1/spotify/src/main/java/cl/duoc/pgy2121/spotify/Spotify.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package cl.duoc.pgy2121.spotify;
+
+/**
+ *
+ * @author Cetecom
+ */
+public class Spotify {
+    public static void main(String[] args) {
+        
+        char sexo = 'N';
+            String nombre = "Sebastian";
+            
+        boolean isValid = false;
+            byte bytes;
+            
+        int edad = 33;
+            float decimal = 3.14f;
+            double doble = 3.10d;
+            short numeroChiquito = 34;
+            
+        System.out.println(nombre);
+        System.out.println(edad);
+        System.out.println("Activo:" + isValid);
+    }
+}
